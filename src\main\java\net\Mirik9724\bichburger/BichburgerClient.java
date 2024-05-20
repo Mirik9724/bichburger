e9995e2e5ad0f23b703ef946d7ed28590ee837ef
Mirik9724 <145905797+Mirik9724@users.noreply.github.com>
2024-05-20 21:31:00 +1000

    Add files via upload

---
+++ src\main\java\net\Mirik9724\bichburger/BichburgerClient.java
@@ -0,0 +1,8 @@
+package net.Mirik9724.bichburger;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BichburgerClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() { }
+}
