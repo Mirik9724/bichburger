293fa8e7523982f7d1c25f7e019a7ae55a5fd198
Mirik9724 <145905797+Mirik9724@users.noreply.github.com>
2024-05-24 12:35:32 +1000

    Delete src/main directory

--- src/main/java/net/Mirik9724/bichburger/BichburgerClient.java
+++
@@ -1,8 +0,0 @@
-package net.Mirik9724.bichburger;
-
-import net.fabricmc.api.ClientModInitializer;
-
-public class BichburgerClient implements ClientModInitializer {
-    @Override
-    public void onInitializeClient() { }
-}
